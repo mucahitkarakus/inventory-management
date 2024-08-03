7f318d51b7c5e6a4d0d2e190f5f420caddf97ae7
mucahitkarakus <mocococo.80@gmail.com>
2024-08-03 17:38:11 +0300

    hamburger menu open close done

--- client/src/app/(components)/Sidebar/page.tsx
+++ client/src/app/(components)/Sidebar/page.tsx
@@ -1,31 +1,66 @@
 "use client"
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
 import { Menu } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
   return (
-    <div>
-    <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
-        <div>
-            logo
-        </div>
-        <h1 className='font-extrabold text-2xl'> 
-            EDSTOCK
+    <div className={sidebarClassNames}>
+      {/* TOP LOGO */}
+      <div
+        className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+          isSidebarCollapsed ? "px-5" : "px-8"
+        }`}
+      >
+        <Image
+          src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
+          alt="edstock-logo"
+          width={27}
+          height={27}
+          className="rounded w-8"
+        />
+        <h1
+          className={`${
+            isSidebarCollapsed ? "hidden" : "block"
+          } font-extrabold text-2xl`}
+        >
+          EDSTOCK
         </h1>
-    <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={()=> {}}>
-      <Menu className='w-4 h-4' />
-    </button>
+
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+
+      {/* LINKS */}
+      <div className="flex-grow mt-8">
+      
+      </div>
+
+      {/* FOOTER */}
+      <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
+      </div>
     </div>
-    {/* Links */}
-    <div className='flex-grow mt-8'>
-    {/* Links here */}
-    </div>
-    {/* footer */}
-    <div>
-      <p className='text-center text-xs text-gray-500'>&copy; 2024 Stock</p>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Sidebar
